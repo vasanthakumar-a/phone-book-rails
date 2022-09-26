b6ed2faa783324901e33adf84575b1ab3e39bb90
Vasanthakumar <vasanthakumara117@gmail.com>
2022-09-26 13:10:03 +0530

    added edit feature

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -21,6 +21,20 @@ class ContactsController < ApplicationController
     end
   end
 
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    if @contact.update(contact_params)
+      redirect_to @contact
+    else
+      render :edit
+    end
+  end
+
   private
   def contact_params
     params.require(:contact).permit(:first_name, :last_name, :phone_number, :email, :company_name, :birth_date, :address)
