353444611ea895fc91b5268d5a1e40d26c76d1d5
Vasanthakumar <vasanthakumara117@gmail.com>
2022-09-26 12:50:39 +0530

    validating field

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true, length: 10
+  validates :email, presence: true
+  validates :company_name, presence: true
+  validates :address, presence: true, length: { minimum: 10 }
 end
