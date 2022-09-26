cdb3692c9203feeb4ef81f99b107ad5bf9aced47
Vasanthakumar <vasanthakumara117@gmail.com>
2022-09-26 14:57:07 +0530

    completed actions

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :phone_number, presence: true, length: { minimum: 10, maximum: 10 }
+  validates :phone_number, presence: true, length: { is: 10 }
   validates :email, presence: true
   validates :company_name, presence: true
   validates :address, presence: true, length: { minimum: 10 }
